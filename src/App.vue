e40f7d721f96d3183918621b00dc8160627e081d
Toby Ryuk <toby.ryuk@danielwellington.com>
2018-04-10 11:50:27 +0200

    :sparkles: Add fetching of stargazers

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app">
-    <h1>Soon</h1>
+    <h1 v-if="vue > react">🎉 YES 🎉</h1>
+    <h1 v-else>Soon...</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      react: 0,
+      vue: 0
+    }
+  },
+  beforeCreate () {
+    fetch('https://api.github.com/repos/facebook/react')
+      .then(response => response.json())
+      .then(data => { this.react = data.stargazers_count })
+    fetch('https://api.github.com/repos/vuejs/vue')
+      .then(response => response.json())
+      .then(data => { this.vue = data.stargazers_count })
+  }
 }
 </script>
