032cfb02bd9b323d2a54880ad66367a46ffb0c5b
Alessio Maffeis <alessio.maffeis@me.com>
2018-06-14 18:59:18 +0200

    :lipstick: Add padding, min-height

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,8 @@ export default {
     color: white;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     width: 100vw;
-    height: 100vh;
+    min-height: 100vh;
+    padding: 2em 0;
     display: flex;
     justify-content: center;
     align-items: center;
